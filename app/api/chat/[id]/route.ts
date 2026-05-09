3b571b9e53281e8a69ef3cc8b0c585eac3e02053
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2026-05-10 00:00:07 +0530

    phase 7 completed4yyyy767676

--- app/api/chat/[id]/route.ts
+++ app/api/chat/[id]/route.ts
@@ -1,37 +1,45 @@
- import { NextResponse } from "next/server";
-import { connectDB } from "@/lib/db";
-import Chat from "@/models/Chat";
+//  import { NextResponse } from "next/server";
+// import { connectDB } from "@/lib/db";
+// import Chat from "@/models/Chat";
 
-export async function GET(
-  request: Request,
-  context: {
-    params: {
-      ticketId: string;
-    };
-  }
-) {
-  try {
-    await connectDB();
+// export async function GET(
+//   request: Request,
+//   context: {
+//     params: {
+//       ticketId: string;
+//     };
+//   }
+// ) {
+//   try {
+//     await connectDB();
 
-    const chats = await Chat.find({
-      ticketId: context.params.ticketId,
-    }).sort({
-      createdAt: 1,
-    });
+//     const chats = await Chat.find({
+//       ticketId: context.params.ticketId,
+//     }).sort({
+//       createdAt: 1,
+//     });
 
-    return NextResponse.json({
-      success: true,
-      data: chats,
-    });
-  } catch (error) {
-    console.log("CHAT FETCH ERROR:", error);
+//     return NextResponse.json({
+//       success: true,
+//       data: chats,
+//     });
+//   } catch (error) {
+//     console.log("CHAT FETCH ERROR:", error);
 
-    return NextResponse.json(
-      {
-        success: false,
-        message: "Failed to fetch chats",
-      },
-      { status: 500 }
-    );
-  }
+//     return NextResponse.json(
+//       {
+//         success: false,
+//         message: "Failed to fetch chats",
+//       },
+//       { status: 500 }
+//     );
+//   }
+// }
+
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({
+    success: true,
+  });
 }
