b4ca39b3fed76da7c85862ba6c021fc35bb91f31
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2026-05-09 23:52:53 +0530

    feed hh

--- app/api/chat/[id]/route.ts
+++ app/api/chat/[id]/route.ts
@@ -1,14 +1,10 @@
-import { NextResponse } from "next/server";
-
+ import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/db";
-
 import Chat from "@/models/Chat";
 
 export async function GET(
-  req: Request,
-  {
-    params,
-  }: {
+  request: Request,
+  context: {
     params: {
       ticketId: string;
     };
@@ -17,30 +13,23 @@ export async function GET(
   try {
     await connectDB();
 
-    const chats =
-      await Chat.find({
-        ticketId:
-          params.ticketId,
-      }).sort({
-        createdAt: 1,
-      });
+    const chats = await Chat.find({
+      ticketId: context.params.ticketId,
+    }).sort({
+      createdAt: 1,
+    });
 
     return NextResponse.json({
       success: true,
-
       data: chats,
     });
-  } catch (error: any) {
-    console.log(
-      "CHAT FETCH ERROR:",
-      error
-    );
+  } catch (error) {
+    console.log("CHAT FETCH ERROR:", error);
 
     return NextResponse.json(
       {
         success: false,
-        message:
-          "Failed to fetch chats",
+        message: "Failed to fetch chats",
       },
       { status: 500 }
     );
