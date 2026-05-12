81b0cb82b4622bf1bb5b9e07f333575b59ac1fcb
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2026-05-12 18:46:51 +0530

    flow issue solvehhffddddd

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,3 +1,40 @@
+// import {
+//   NextRequest,
+//   NextResponse,
+// } from "next/server";
+
+// import { elastic } from "@/lib/elasticsearch";
+
+// export async function GET(
+//   req: NextRequest
+// ) {
+//   const query =
+//     req.nextUrl.searchParams.get(
+//       "q"
+//     );
+
+//   const result =
+//     await elastic.search({
+//       index:
+//         "tickets",
+
+//       query: {
+//         multi_match: {
+//           query,
+
+//           fields: [
+//             "title",
+//             "description",
+//             "customerName",
+//           ],
+//         },
+//       },
+//     });
+
+//   return NextResponse.json(
+//     result.hits.hits
+//   );
+// }
 import {
   NextRequest,
   NextResponse,
@@ -9,14 +46,11 @@ export async function GET(
   req: NextRequest
 ) {
   const query =
-    req.nextUrl.searchParams.get(
-      "q"
-    );
+    req.nextUrl.searchParams.get("q") || "";
 
   const result =
     await elastic.search({
-      index:
-        "tickets",
+      index: "tickets",
 
       query: {
         multi_match: {
