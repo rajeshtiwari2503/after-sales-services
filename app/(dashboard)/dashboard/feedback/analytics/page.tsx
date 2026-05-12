f333d9e3d437f1d4a3d83216ed6fbeb7a682130a
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2026-05-12 17:25:10 +0530

    flow progress 1 vercel error solve

---
+++ app/(dashboard)/dashboard/feedback/analytics/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div className="space-y-6">
+      <h1 className="text-3xl font-black">
+        Feedback analytics
+      </h1>
+
+     
+    </div>
+  );
+}
