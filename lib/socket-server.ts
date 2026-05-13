08246a8585563eefa077ddb1049d4bba03dd5941
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2026-05-13 11:46:23 +0530

    error solvehhmmmmmggddh

--- lib/socket-server.ts
+++ lib/socket-server.ts
@@ -1,48 +1,71 @@
-const { Server } =
-  require("socket.io");
+// const { Server } =
+//   require("socket.io");
 
-const http =
-  require("http");
+// const http =
+//   require("http");
 
-const server =
-  http.createServer();
+// const server =
+//   http.createServer();
 
-const io = new Server(
-  server,
-  {
-    cors: {
-      origin: "*",
-    },
-  }
-);
+// const io = new Server(
+//   server,
+//   {
+//     cors: {
+//       origin: "*",
+//     },
+//   }
+// );
 
-io.on(
-  "connection",
-  (socket) => {
-    console.log(
-      "Client connected"
-    );
+// io.on(
+//   "connection",
+//   (socket) => {
+//     console.log(
+//       "Client connected"
+//     );
 
-    socket.on(
-      "disconnect",
-      () => {
-        console.log(
-          "Disconnected"
-        );
-      }
-    );
-  }
-);
+//     socket.on(
+//       "disconnect",
+//       () => {
+//         console.log(
+//           "Disconnected"
+//         );
+//       }
+//     );
+//   }
+// );
 
-server.listen(
-  3001,
-  () => {
-    console.log(
-      "Socket Server Running"
-    );
-  }
-);
+// server.listen(
+//   3001,
+//   () => {
+//     console.log(
+//       "Socket Server Running"
+//     );
+//   }
+// );
 
-// "scripts": {
-//   "socket": "node src/server/socket-server.js"
-// }
+// // "scripts": {
+// //   "socket": "node src/server/socket-server.js"
+// // }
+
+import http from "http";
+import { Server, Socket } from "socket.io";
+
+const server = http.createServer();
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket: Socket) => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected:", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Socket Server Running on 3001");
+});
